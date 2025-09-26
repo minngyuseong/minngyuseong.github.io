14b872576b1343897b9fdf6e4d94dd4059dbb267
Mingyu <minngyuseong@gmail.com>
2025-09-27 00:28:58 +0900

    feat: update font and metadata

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,16 +9,13 @@ const navItems: NavItem[] = [
 ];
 
 export default function Header() {
-  // 함수 컴포넌트이므로 React.FC를 사용할 수도 있으나, 여기서는 일반 함수 구문 사용
   return (
     <header className="fixed top-0 left-0 z-50 w-full bg-white/95 shadow-sm">
       <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-        {/* 로고 / 타이틀 */}
         <Link href="/" className="text-xl font-bold text-[#333]">
-          SOYOUNG&apos;S PORTFOLIO
+          MINGYU&apos;S PORTFOLIO
         </Link>
 
-        {/* 내비게이션 링크 */}
         <nav className="hidden md:block">
           <ul className="flex space-x-6">
             {navItems.map(
